3714371fae846f55d2070bf98851639e7712b4dc
imbanqueri <banquerif@gmail.com>
2020-10-26 08:34:22 +0100

    Views (Template Engine)
    Views (Template Engine)

--- src/database.js
+++ src/database.js
@@ -4,6 +4,7 @@ mongoose.connect('mongodb://localhost/notes-db-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err));
