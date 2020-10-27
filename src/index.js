495f9f265a857f971568dc832443ae870260f363
imbanqueri <banquerif@gmail.com>
2020-10-27 14:41:05 +0100

    CRUD (Create, Read, Update, Delete)
    CRUD (Create, Read, Update, Delete)

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
-const session = require('express-session')
+const session = require('express-session');
+const flash = require('connect-flash')
+const handlebars = require('handlebars');
+const {allowInsecurePrototypeAcces, allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 
 /* Initiliazations (Inicializaciones) */
 const app = express();
 require('./database');
 
 /* Settings (Configuraciones) */
-app.set('port', process.env.PORT || 1957);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main.hbs',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
+    handlebars: allowInsecurePrototypeAccess(handlebars)
 }));
 app.set('view engine', '.hbs');
 
@@ -27,8 +31,14 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
 }))
+app.use(flash());
 
 /* Global Variables (Variables globales) (Datos accesibles para toda nuestra aplicacion) */
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 /* Routes (Rutas) */
 app.use(require('./routes/index'));
